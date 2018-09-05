fe00c9c822cf3d82f4d8bb87dd44bcb798f750b3
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-09-05 15:38:11 -0400

    Update

--- Ch02_prj2_GradeConverter/src/GradeConverterApp.java
+++ Ch02_prj2_GradeConverter/src/GradeConverterApp.java
@@ -3,37 +3,39 @@ import java.util.Scanner;
 public class GradeConverterApp {
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to the Letter Grade Converter");
-	
-	Scanner sc = new Scanner (System.in);
-	
-	System.out.print("Enter numerical grade: ");
-	int numericalGrade = sc.nextInt();
-	
-	String letterGrade = "";
-	
-	// Convert the numeric grade to letter grade
-	if (numericalGrade>=88) {
-		letterGrade = "A";
-		
-	}
-	else if (numericalGrade>=80) {
-		letterGrade = "B";
-	}
-	else if (numericalGrade>=68) {
-		letterGrade = "C";
-	}
-	else if (numericalGrade>=60) {
-		letterGrade = "D";
-	}
-	// else without the if catches everything else that is entered
-	else {
-		letterGrade = "F";
-	}
-	System.out.print ("Letter Grade: "+letterGrade);
-	
-	sc.close();		
-	System.out.println("Bye");
+		System.out.println("Welcome to the Letter Grade Converter");
+		Scanner sc = new Scanner(System.in);
+
+		String choice = "y";
+
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter numerical grade: ");
+			int numericalGrade = sc.nextInt();
+
+			String letterGrade = "";
+
+			// Convert the numeric grade to letter grade
+			if (numericalGrade >= 88) {
+				letterGrade = "A";
+
+			} else if (numericalGrade >= 80) {
+				letterGrade = "B";
+			} else if (numericalGrade >= 68) {
+				letterGrade = "C";
+			} else if (numericalGrade >= 60) {
+				letterGrade = "D";
+			}
+			// else without the if catches everything else that is entered
+			else {
+				letterGrade = "F";
+			}
+			System.out.println("Letter Grade: " + letterGrade);
+			
+			System.out.print("Continue y/n ");
+			choice = sc.next();
+		}
+		sc.close();
+		System.out.println("Bye");
 	}
 
 }
